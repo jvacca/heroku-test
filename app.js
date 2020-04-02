a2bb062cf43e9f3f721b58714562cbc25de20765
jvacca <jcvacca@gmail.com>
2020-04-02 19:29:07 -0400

    fixing a error2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   console.log("root rendering")
   //res.render('server_side_template', siteData);
-  res.render('main', {layout: 'index', apiData:siteData});
+  res.render('main', {layout: 'index'});
 });
 
 app.listen(app.get('port'), function() {
